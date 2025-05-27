48ca5cb2c69aa503867ab739087ee3d82335203f
siva <siva@kmdigicommerce.com>
2025-05-27 18:17:53 +0530

    optimize revenue widget

--- ecommerce_tool/omnisight/tasks.py
+++ ecommerce_tool/omnisight/tasks.py
@@ -1,11 +1,14 @@
 from celery import shared_task
 from omnisight.operations.walmart_operations import syncRecentWalmartOrders
-from omnisight.operations.amazon_operations import syncRecentAmazonOrders
+from omnisight.operations.amazon_operations import syncRecentAmazonOrders,sync_inventory
 
 @shared_task
 def sync_orders():
     print("Orders Sync starting........................")
     walmart_orders = syncRecentWalmartOrders()
     amazon_orders = syncRecentAmazonOrders()
+    print("Orders Sync completed........................")
+    sync_inventory()
+    print("Inventory Sync completed........................")
     
     return f"Synced {len(walmart_orders) + len(amazon_orders)} orders"
