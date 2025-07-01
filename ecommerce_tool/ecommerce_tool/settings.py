e73ad786702c85396ff685ebd2ba46910f853ee7
Harisankar <harisankar@uniqnex360.com>
2025-07-01 17:09:03 +0530

    cors issue resolved

--- ecommerce_tool/ecommerce_tool/settings.py
+++ ecommerce_tool/ecommerce_tool/settings.py
@@ -44,6 +44,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
+     "https://34.195.154.218",
     "http://192.168.30.191:4200",
     "https://b2bop.netlify.app"
 ]
